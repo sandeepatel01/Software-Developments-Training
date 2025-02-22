fee0e3e66956c7cffb557ff93940f2c192823072
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-02-22 10:26:54 +0530

    Done: JS DOM Challenge -2

--- JavaScript/Challenges/DOM/DOM Challaiges [1-5]/challenge-2/app.js
+++ JavaScript/Challenges/DOM/DOM Challaiges [1-5]/challenge-2/app.js
@@ -1,3 +1,27 @@
-/**
- * Write your challenge solution here
- */
+const redButton = document.getElementById("redButton");
+const greenButton = document.getElementById("greenButton");
+const blueButton = document.getElementById("blueButton");
+const purpleButton = document.getElementById("purpleButton");
+const resetButton = document.getElementById("resetButton");
+
+const mainHeading = document.getElementById("mainHeading");
+
+redButton.addEventListener('click', () => {
+  mainHeading.style.color = "#e74c3c";
+});
+
+greenButton.addEventListener('click', () => {
+  mainHeading.style.color = "#2ecc71";
+});
+
+blueButton.addEventListener('click', () => {
+  mainHeading.style.color = "#3498db";
+});
+
+purpleButton.addEventListener('click', () => {
+  mainHeading.style.color = "#9b59b6";
+});
+
+resetButton.addEventListener('click', () => {
+  mainHeading.style.color = "";
+});
