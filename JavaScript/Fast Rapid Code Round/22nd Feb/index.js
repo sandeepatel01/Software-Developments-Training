7993dc6c61bcd6e733c8946fa6f87ead81386386
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-02-23 21:25:46 +0530

    Start JS Marathon

---
+++ JavaScript/Fast Rapid Code Round/22nd Feb/index.js
@@ -0,0 +1,8 @@
+// var name = "Sandy";  // Not Recomonded
+
+let name = "Sandeep";
+const age = 23;
+
+let number = 40; // Number
+let text = "Sandy"; // String
+let isTrue = true; // Boolean
---
+++ JavaScript/JS Marathon/22 Feb/01_variables_datatypes.js
@@ -0,0 +1,91 @@
+// ******************** VARIABLES *************************
+
+var name = "Sandy"; // Not Recommended
+let name1 = "Sandy"; // Recommended
+const pi = 3.14; // Recommended
+
+
+// *************************** DATA TYPES ***********************
+
+// ****** Primitive Data Types ****
+let number = 10; // Number
+let text = "Hello"; // String
+let isTrue = true; // Boolean 
+
+let nothing = null; // Object  [Null of data type is Null] 
+console.log(typeof nothing); // object
+
+let undefiendVar = undefined; // Undefined
+console.log(typeof undefiendVar); // undefined
+
+let symbolVar = Symbol(); // Symbol
+console.log(typeof symbolVar); // symbol
+
+
+// ****** Reference Data Types ************
+
+let person = {
+  name: "Sandy",
+  age: 20,
+  isWorkingProfessional: true
+}
+
+// ****** Data Type Conversion ************
+
+let num = "10"; // "10abc"
+// let convertedNum = Number(num); // Recommended
+// let convertedNum = +num;
+let convertedNum = parseInt(num);
+
+console.log(convertedNum); // 10,   NaN
+console.log(typeof convertedNum); // number
+
+
+let str = 10;
+// let convertedStr = String(str); // Recommended
+let convertedStr = str.toString();
+
+console.log(convertedStr); // "10"
+console.log(typeof convertedStr); // string
+
+
+// ****** Operators ************
+
+let a = 10;
+let b = 20;
+
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+let remainder = a % b;
+let power = a ** b;
+
+console.log(sum); // 30
+console.log(difference); // -10
+console.log(product); // 200
+console.log(quotient); // 0.5
+console.log(remainder); // 10
+console.log(power); // 100000
+
+
+// ****** Comparison **********
+
+let x = 10;
+let y = 20;
+
+// == (Equal to)  - Checks only data
+// === (Strict equal to) - Checks data and data type
+// != (Not equal to)
+// > (Greater than)
+// < (Less than)
+// >= (Greater than or equal to)
+// <= (Less than or equal to)
+
+console.log(x == y); // false
+console.log(x === y); // false
+console.log(x != y); // true
+console.log(x > y); // false
+console.log(x < y); // true
+console.log(x >= y); // false
+console.log(x <= y); // true
